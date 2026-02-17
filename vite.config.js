855c3f83bf63a8c4137a2073aa8931494a89f5c8
jaet65 <agrazmario@outlook.com>
2026-02-17 15:37:32 -0600

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // ESTA SECCIÓN ES LA CLAVE PARA "npm run dev"
+      devOptions: {
+        enabled: true, // Habilita la PWA en desarrollo
+        type: 'module', // Permite usar módulos en el service worker si es necesario
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'TrackSIM Academy',
+        short_name: 'TrackSIM',
+        description: 'Plataforma de Evaluación y Certificación DC-3',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     headers: {
-      // ESTAS 2 LÍNEAS ARREGLAN EL ERROR DEL POPUP
       "Cross-Origin-Embedder-Policy": "unsafe-none",
       "Cross-Origin-Opener-Policy": "unsafe-none",
     },
